bc70353457b02d5df19796501c4bc04bc647e56b
Michał Augustyniak <michal.augustyniak123@gmail.com>
2024-12-30 19:30:38 +0100

    add FileNotFoundException, NotImplementedException to the exception handler

--- src/main/java/com/pm/personnelmanagement/common/handler/ApiExceptionHandler.java
+++ src/main/java/com/pm/personnelmanagement/common/handler/ApiExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.pm.personnelmanagement.common.handler;
 
 import com.pm.personnelmanagement.common.dto.ApiException;
+import com.pm.personnelmanagement.file.exception.FileNotFoundException;
+import com.pm.personnelmanagement.file.exception.NotImplementedException;
 import com.pm.personnelmanagement.task.exception.TaskNotFoundException;
 import com.pm.personnelmanagement.taskevent.expcetion.TaskEventNotFoundException;
 import org.springframework.http.HttpStatus;
@@ -27,6 +29,28 @@ public class ApiExceptionHandler {
     @ExceptionHandler(value = TaskEventNotFoundException.class)
     public ResponseEntity<Object> taskEventNotFoundException(TaskEventNotFoundException e) {
         HttpStatus status = HttpStatus.NOT_FOUND;
+        ApiException exception = new ApiException(
+                e.getMessage(),
+                status.value(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(exception, status);
+    }
+
+    @ExceptionHandler(value = FileNotFoundException.class)
+    public ResponseEntity<Object> fileNotFoundException(FileNotFoundException e) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        ApiException exception = new ApiException(
+                e.getMessage(),
+                status.value(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(exception, status);
+    }
+
+    @ExceptionHandler(value = NotImplementedException.class)
+    public ResponseEntity<Object> notImplementedException(NotImplementedException e) {
+        HttpStatus status = HttpStatus.NOT_IMPLEMENTED;
         ApiException exception = new ApiException(
                 e.getMessage(),
                 status.value(),
